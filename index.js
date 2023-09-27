bf5b57a4c25d09a65b66d0bbd6afe18cb96e55f6
ravine13 <kuriaderek@gmail.com>
2023-09-27 10:57:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+//const hey = () => {
+    //return "hey";
+//}
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "L"},
+    { year: "2013", result: "L"},
+    ];
+  
+function superbowlWin(record){
+const winningGame = record.find(game => game.result === "W")
+if(winningGame){
+    return winningGame.year;
+} else{
+    return undefined;
+}
+}
+
+const winningYear = superbowlWin(record);
+console.log(` ${winningYear}`);
